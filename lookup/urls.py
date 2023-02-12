35eb3280e8831713a10bc296aaf03bf8467ba4a3
Austin Holmes <austin.holmes4@gmail.com>
2023-02-12 09:20:44 -0700

    bootstrap added and navbar done

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from lookup import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+
 ]
